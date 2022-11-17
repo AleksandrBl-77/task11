58aa2b710cc4fcd8c8f353d5d4589ac10a3d7954
Aleksandr <tkdespadon@mail.ru>
2022-11-18 00:17:06 +0300

    task13-13

---
+++ task10/Program.cs
@@ -0,0 +1,12 @@
+﻿int random = new Random().Next(100, 1000);
+Console.WriteLine(random);
+
+int NumberTwo(int ran)
+{
+    int number2 = ((random / 10) % 10);
+    return number2;
+}
+
+int numberTwo = NumberTwo(random);
+
+Console.WriteLine(numberTwo);
