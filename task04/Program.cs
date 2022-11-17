3e51e60e4585b9ff77d54718f33c8fe1bdb74864
Aleksandr <tkdespadon@mail.ru>
2022-11-18 00:16:02 +0300

    task13-13

---
+++ task04/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.Write("Введите число а: ");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите число c: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write($"Максимальное число {max}");
