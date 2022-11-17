2d6f5d23f464b8e4dfd2caef5dc4819fc8bd0fd8
Aleksandr <tkdespadon@mail.ru>
2022-11-18 00:13:47 +0300

    task13-13

---
+++ task02/Program.cs
@@ -0,0 +1,8 @@
+﻿System.Console.Write("Введите число а: ");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a < b ){
+    Console.Write($"Число b({b}) бельше, число a({a}) меньше ");
+} else
+Console.Write($"Число a({a}) больше, число b({b})меньше "); 
