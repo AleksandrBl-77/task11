17791accbbd51f4d6e141685ad73b0cf5e16c2f0
Aleksandr <tkdespadon@mail.ru>
2022-11-18 00:13:14 +0300

    task13-13

---
+++ task13/Program.cs
@@ -0,0 +1,14 @@
+﻿System.Console.Write("Введите число: ");
+void determinesThird()
+{
+    string num = Console.ReadLine();
+    char result = num[2];
+    if(num == null) {
+        Console.WriteLine("нет числа");
+    }
+    else
+    {
+      Console.WriteLine(result);  
+    }
+}
+determinesThird();
