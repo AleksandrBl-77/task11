77334a27802cdf278e41d5fcea772479a80216d8
Aleksandr <tkdespadon@mail.ru>
2022-11-18 00:17:26 +0300

    task13-13

---
+++ task15/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите номер дня недели от 1 до 7: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void knowdayOff()
+
+{
+    if (number <= 5 && number > 0)
+    {
+        Console.WriteLine("нет");
+    }
+    if (number > 5 && number <= 7)
+    {
+        Console.WriteLine("да");
+    }
+    if (number > 7)
+    {
+        Console.WriteLine($"{number} это некорректные данные ");
+    }
+}
+knowdayOff();
