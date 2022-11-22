5a99684af2d084b46257e18813e8f19373987ca2
Aleksandr <tkdespadon@mail.ru>
2022-11-22 14:29:53 +0300

    task23

---
+++ task23/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число N:  ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+string findСube(int N)
+{
+    string result = "";
+    for (int i = 1; i <= N; i++)
+    {
+        result += $"{Math.Pow(i, 3)} ";
+    }
+    return result;
+}
+
+Console.WriteLine(findСube(N));
