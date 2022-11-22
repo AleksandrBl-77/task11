8eb5fb4b5c11d4d66067f871624c390becc8ab1a
Aleksandr <tkdespadon@mail.ru>
2022-11-22 13:58:32 +0300

    task19

---
+++ Task19/Program.cs
@@ -0,0 +1,26 @@
+﻿int input = new Random().Next(10000, 100000);
+
+Console.WriteLine(input);
+
+int FindPalindrom(int number)
+{
+	int rev_num = 0;
+	int origin_num = number;
+	while(origin_num > 0)
+	{
+		rev_num = (rev_num *10) + origin_num % 10;
+		origin_num = origin_num / 10;
+
+	}
+	return rev_num;
+}
+
+int Reverse = FindPalindrom(input);
+if(Reverse == input)
+{
+	Console.Write("Palindrom");
+}
+else
+{
+	Console.Write("Not Palindrom");
+}
