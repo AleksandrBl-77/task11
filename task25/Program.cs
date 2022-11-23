71e3ece049e36598a2c531192c576c099380448b
Aleksandr <tkdespadon@mail.ru>
2022-11-23 15:44:25 +0300

    task25

---
+++ task25/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число:  ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите степень:  ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+double FindDegree(int x, int y)
+{
+    double degree = Math.Pow(x , y);
+
+   return degree;
+}
+double result = FindDegree(x,y);
+Console.WriteLine(result);
