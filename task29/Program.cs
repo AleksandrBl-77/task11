6212e78d18c763dd81735d8cec242a30baebfca0
Aleksandr <tkdespadon@mail.ru>
2022-11-23 15:52:37 +0300

    task29

---
+++ task29/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] array = new int[8];
+
+int[] DefinesArray(int[] arr) 
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+    arr[i] = new Random().Next(0, 100);
+    }
+   
+    return arr;
+}
+
+void PtintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        System.Console.Write($"{arr[i]} ");
+    }
+}
+int[] n = DefinesArray(array);
+PtintArray(n);
