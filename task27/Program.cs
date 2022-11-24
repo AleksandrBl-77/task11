f3a6e77070a7f745acfaec2e897fd181b9ab5e2c
Aleksandr <tkdespadon@mail.ru>
2022-11-24 10:38:19 +0300

    task27

---
+++ task27/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите целое число:  ");
+var n = Convert.ToInt32(Console.ReadLine());
+
+int SumDigitsm(int num)
+{
+    var sum = 0;
+    while (n > 0)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    return sum;
+}
+int result = SumDigitsm(n);
+
+Console.WriteLine(result);
