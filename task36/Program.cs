9f9e8e8cccd38986400c7e30207bba55dc426408
Aleksandr <tkdespadon@mail.ru>
2022-11-27 17:20:07 +0300

    task36

---
+++ task36/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] arr = new int[4];
+
+Random rnd = new Random();
+
+Console.WriteLine("Исходный массив");
+for (int i = 0; i < 4; i++)
+{
+    arr[i] = rnd.Next(-99, 100);
+    Console.Write("{0} ", arr[i]);
+}
+
+var even = arr.Where((element, index) => index % 2 != 0);
+Console.WriteLine("Сумма четных: {1}.", even.Count(), even.Sum());
