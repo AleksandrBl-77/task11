df810e8a4b1d79e561ae75117279a01d43aaf4ec
Aleksandr <tkdespadon@mail.ru>
2022-11-28 11:45:22 +0300

    task38-2

--- task38/Program.cs
+++ task38/Program.cs
@@ -9,8 +9,8 @@ for (int i = 0; i < d.Length; i++)
     if (d[i] < min) min = d[i];
 }
 
-for (int i = 0; i < d.Length; i++) Console.Write(" " + d[i]);
+for (int i = 0; i < d.Length; i++) Console.Write($" "+ d[i]);
 Console.WriteLine();
 Console.WriteLine("max:  " + max);
-Console.WriteLine("min  " + min);
+Console.WriteLine("min:  " + min);
 Console.WriteLine("Разница максимального и минимального:  " + (max - min));
