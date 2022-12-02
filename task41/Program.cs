20b0fe5c396c55b804fa64be262fca6fa4086213
Aleksandr <tkdespadon@mail.ru>
2022-12-02 20:14:51 +0300

    task41

---
+++ task41/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.Write("Введите элементы(через пробел): ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+Console.WriteLine($"Кол-во элементов > 0: {CountOfElems(arr)}");
+
+int CountOfElems(int[] array)
+{
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+if (array[i] > 0)
+{
+count++;
+}
+}
+
+return count;
+}
