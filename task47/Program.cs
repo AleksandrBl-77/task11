bd3ea768010b1b0314832df3d0f52958cbbecae0
Aleksandr <tkdespadon@mail.ru>
2022-12-05 21:03:16 +0300

    task47

---
+++ task47/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите колличество строк ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите колличество столбцов ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+double[,] arrayTable = new double[num1, num2];
+var random = new Random();
+
+for (int i =0; i < arrayTable.GetLength(0); i++)
+    {
+       for (int j =0; j < arrayTable.GetLength(1); j++) 
+       {
+        arrayTable[i,j] = random.NextDouble()* 10; 
+
+            Console.Write($"{Math.Round(arrayTable[i, j], 1)}   ");
+       }
+       Console.WriteLine();
+    }
