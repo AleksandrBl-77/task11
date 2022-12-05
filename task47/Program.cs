89dbe4aa64dbf0b8848a887d1629c123f610d42e
Aleksandr <tkdespadon@mail.ru>
2022-12-05 21:15:07 +0300

    task47-2

--- task47/Program.cs
+++ task47/Program.cs
@@ -6,6 +6,9 @@ int num2 = Convert.ToInt32(Console.ReadLine());
 double[,] arrayTable = new double[num1, num2];
 var random = new Random();
 
+ArrayTable (num1, num2);
+void ArrayTable (int num1, int num2)
+{
 for (int i =0; i < arrayTable.GetLength(0); i++)
     {
        for (int j =0; j < arrayTable.GetLength(1); j++) 
@@ -16,3 +19,4 @@ for (int i =0; i < arrayTable.GetLength(0); i++)
        }
        Console.WriteLine();
     }
+}
